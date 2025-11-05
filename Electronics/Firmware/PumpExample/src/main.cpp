f3b6ab08256dca31e0cc2cbf5a67775957646e82
Sebastian <andersen_sebastian@yahoo.dk>
2025-11-05 13:25:12 +0100

    Update main.cpp

--- Electronics/Firmware/PumpExample/src/main.cpp
+++ Electronics/Firmware/PumpExample/src/main.cpp
@@ -2,6 +2,8 @@
 #include "dataModel.h"
 #include "df4MotorDriver.h"
 
+int full_speed = 4096; // Full speed for the pumps
+
 void setup() {
   Serial.begin(115200);
   Serial.println("Setup started");
@@ -12,11 +14,15 @@ void setup() {
 void loop() {
   delay(3000);
   Serial.println("Starting pump A forward at full speed for 3 seconds");
-  StartPumpA(4096, HIGH);
+  StartPumpA(full_speed, HIGH);
 
   delay(3000);
   Serial.println("Starting pump A forward at half speed for 3 seconds");
-  StartPumpA(2048, HIGH);
+  StartPumpA(full_speed / 2, HIGH);
+
+  delay(3000);
+  Serial.println("Starting pump A backward at half speed for 3 seconds");
+  StartPumpA(full_speed / 2, LOW);
 
   delay(3000);
   stopPumps();
